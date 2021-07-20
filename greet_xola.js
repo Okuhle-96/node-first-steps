902cc43b1118570c71abb89e83456e3725e2917f
Okuhle_Mhambi <bukhosimhambi@gmail.com>
2021-07-20 05:59:42 +0200

    tyh

--- greet_xola.js
+++ greet_xola.js
@@ -2,5 +2,16 @@ const chalk = require('chalk');
 //import the greet module that is in the current folder
 const greet = require('./greet');
 
+var figlet = require('figlet');
+ 
+figlet('Hello Xola!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
